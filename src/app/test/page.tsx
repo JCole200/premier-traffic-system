f2704c35addbb1e521fdb686fe4d71a55c5e3097
JCole200 <judah.cole@premier.org.uk>
2026-02-03 09:00:54 +0000

    chore: add diagnostic routes to isolate vercel issue

---
+++ src/app/test/page.tsx
@@ -0,0 +1,9 @@
+
+export default function TestPage() {
+    return (
+        <div style={{ padding: '50px', fontFamily: 'sans-serif' }}>
+            <h1>Static Page Test</h1>
+            <p>If you can see this, Next.js routing is working.</p>
+        </div>
+    );
+}
